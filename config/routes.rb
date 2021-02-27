249d882e25c47bd2c0fed8150c17bef43916f784
Philippe Ghestem <philippe_ghestem@hotmail.com>
2021-02-27 13:53:24 +0100

    Création initiale des routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
+
+  get '/info', to: 'pages#information'
+
+  resources :personal_info_users, only: [:edit, :create]
+  resources :company_info_users, only: [:edit, :create]
+
+  namespace :dashboard do
+    resources :invoices
+    resources :taxes, only: [:index]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
