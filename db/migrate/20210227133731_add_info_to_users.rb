6a5f870c8e06c59e11ff06855e02f441ec9f659a
Philippe Ghestem <philippe_ghestem@hotmail.com>
2021-02-28 11:56:59 +0100

    1ère construction model user

---
+++ db/migrate/20210227133731_add_info_to_users.rb
@@ -0,0 +1,23 @@
+class AddInfoToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :use_name, :string
+    add_column :users, :pseudonym, :string
+    add_column :users, :address, :string
+    add_column :users, :city, :string
+    add_column :users, :zipcode, :string
+    add_column :users, :country :string
+    add_column :users, :citizenship, :string
+    add_column :users, :birth_date, :string
+    add_column :users, :birth_city, :string
+    add_column :users, :birth_department, :string
+    add_column :users, :birth_country, :string
+    add_column :users, :gender, :string
+    add_column :users, :email, :string
+    add_column :users, :phone_number, :string
+    add_column :users, :ssn, :string
+    add_column :users, :spouse, :boolean
+
+  end
+end
