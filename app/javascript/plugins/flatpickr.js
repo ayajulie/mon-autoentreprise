f4ce402b980bae9aa40f2a13ef4597b9866b5ef5
Philippe Ghestem <philippe_ghestem@hotmail.com>
2021-03-13 19:36:18 +0100

    page info avec progression du remplissage du formulaire

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,12 +1,18 @@
 import flatpickr from "flatpickr";
+// import "flatpickr/dist/flatpickr.min.css"
+
+import { French } from "flatpickr/dist/l10n/fr.js"
+
+
 
 const initFlatpickr = () => {
   const calendars = document.querySelectorAll(".calendar")
   calendars.forEach((calendar) => {
     flatpickr(".calendar", {
       altInput: true,
-      altFormat: "j F, Y",
+      altFormat: "j F Y",
       dateFormat: "Y-m-d",
+      "locale": French
     });
   });
 };
