a03712859943575c58e905d16b4bd82041df8def
Philippe Ghestem <philippe_ghestem@hotmail.com>
2021-03-13 15:23:21 +0100

    Navigation dashboard

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2021_03_13_092024) do
-=======
-ActiveRecord::Schema.define(version: 2021_03_13_081408) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
