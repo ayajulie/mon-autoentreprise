98bb38731d14491b7100d9a443c1ba98dd0572bc
geoffroy goirand <geoffroy.goirand@gmail.com>
2021-10-12 19:40:00 +0200

    societe2

--- app/controllers/societes_controller.rb
+++ app/controllers/societes_controller.rb
@@ -1,2 +1,16 @@
 class SocietesController < ApplicationController
+
+  def new
+    @societe = Societe.new
+  end
+
+  def create
+  end
+
+  def show
+    @societe = Societe.find(params[:id])
+    respond_to do |format|
+    format.docx do
+      render docx: 'sas.docx.erb', filename: 'statut.docx'
+  end
 end
