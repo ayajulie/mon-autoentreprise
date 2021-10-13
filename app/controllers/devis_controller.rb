042bacd605098bf7b49ea4203dcb725439e112d3
geoffroy goirand <geoffroy.goirand@gmail.com>
2021-10-13 21:22:14 +0200

    relance2

--- app/controllers/devis_controller.rb
+++ app/controllers/devis_controller.rb
@@ -4,15 +4,14 @@ class DevisController < ApplicationController
     @devis = Devi.all
   end
 
-<<<<<<< HEAD
-=======
+
    def show
     @devi=Devi.find(params[:id])
     @devis = Devi.all
   end
 
 
->>>>>>> 2af684f29dca831117579e4b7e4e6c6ae108ce91
+
   def new
    @devi = Devi.new
   end
@@ -20,18 +19,16 @@ class DevisController < ApplicationController
   def create
     @devi = Devi.new(devis_params)
     if @devi.save
-<<<<<<< HEAD
-      redirect_to root_path
-=======
+
+
       redirect_to dashboard_path
->>>>>>> 2af684f29dca831117579e4b7e4e6c6ae108ce91
+
     else
       render "new"
     end
   end
 
-<<<<<<< HEAD
-=======
+
   def edit
      @devi = Devi.find(params[:id])
   end
@@ -47,15 +44,12 @@ class DevisController < ApplicationController
     @devi.destroy
     redirect_to root
   end
->>>>>>> 2af684f29dca831117579e4b7e4e6c6ae108ce91
+
 
   private
 
   def devis_params
-<<<<<<< HEAD
-    params.require(:devi).pemit(:objet, :devis_at, :amount, :success)
-=======
     params.require(:devi).permit(:name, :objet, :devis_at, :amount, :success)
->>>>>>> 2af684f29dca831117579e4b7e4e6c6ae108ce91
+
   end
 end
