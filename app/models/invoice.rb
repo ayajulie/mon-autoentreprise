bd08f584d532946a672c283f2423e073e8a83f58
geoffroy goirand <geoffroy.goirand@gmail.com>
2021-10-19 14:22:29 +0200

    securecompta

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -1,5 +1,6 @@
 class Invoice < ApplicationRecord
   belongs_to :user
   validates  :amount, :invoiced_at, :object, presence: true
+  attribute :amount, :integer, default: 0
   belongs_to :client
 end
