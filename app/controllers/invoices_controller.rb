fbe368934d2247a52857764e7b163103d6eb8cdd
geoffroy goirand <geoffroy.goirand@gmail.com>
2021-10-20 14:59:01 +0200

    securecompta22

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -38,6 +38,7 @@ class InvoicesController < ApplicationController
 
     if @invoices.nil?
       @invoices =0
+      redirect_to new_invoice_path
 
    else
     @invoices = Invoice.all.where(user:current_user)
