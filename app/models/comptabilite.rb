de01b3dea775fd1f92600be8fe5dd20a35d374b9
geoffroy goirand <geoffroy.goirand@gmail.com>
2021-10-22 21:46:45 +0200

    client28

--- app/models/comptabilite.rb
+++ app/models/comptabilite.rb
@@ -1,6 +1,6 @@
 class Comptabilite < ApplicationRecord
   belongs_to :user
   validates :user, uniqueness: true
-  has_many_and_belongs_to :invoices
+  has_and_belongs_to_many :invoices
 
 end
--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,5 +2,5 @@ class Invoice < ApplicationRecord
   belongs_to :user
   validates  :amount, :invoiced_at, :object, presence: true
   attribute :amount, :integer, default: 0
-  has_many_and_belongs_to :comptabilites
+  has_and_belongs_to_many :comptabilites
 end
