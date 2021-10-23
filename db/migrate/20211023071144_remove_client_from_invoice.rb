0377e5c7cc27c2b5901d27ef74421a65035a89de
geoffroy goirand <geoffroy.goirand@gmail.com>
2021-10-23 09:11:59 +0200

    client 34

--- db/migrate/20211023071144_remove_client_from_invoice.rb
+++ db/migrate/20211023071144_remove_client_from_invoice.rb
@@ -1,5 +1,5 @@
 class RemoveClientFromInvoice < ActiveRecord::Migration[6.0]
   def change
-    remove_reference :invoices, :clients, null: false, foreign_key: true
+    remove_reference :invoices, :client, null: false, foreign_key: true
   end
 end
